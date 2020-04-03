3bbd3760038f573f026727b0560aa0c44ddfe3d7
Louis Poncet <louis@poncet.org>
2020-04-03 18:12:59 +0200

    First php page for testing the library

---
+++ backendCode/testlib.php
@@ -0,0 +1,11 @@
+<html>
+<h1>Welcome</h1>
+<p>
+  Insert : joe schmucky joeyboy joe.schmuck@silly.net
+</p>
+<?php
+require 'functionLibrary.php';
+// function insertNewUser($firstName, $lastName, $userName, $emailaddress)
+insertNewUser('joe','schmucky','joeboy','joe.schmuck@silly.net');
+?>
+</html>
