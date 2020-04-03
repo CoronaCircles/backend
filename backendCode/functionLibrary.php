a3b6c527d52d7ed948a1367800dfaed44f8fdded
Louis Poncet <louis@poncet.org>
2020-04-03 16:03:50 +0200

    1st step in porting of the backend

--- backendCode/functionLibrary.php
+++ backendCode/functionLibrary.php
@@ -1,38 +1,35 @@
+<?php
 // port of python code for coronacircles project
 // chris rehm, christopherrehm@web.de apr3 2020
 
 
-<?php
-$servername = "rdbms.strato.de";
-$username = "U4098787";
-$password = "1light1light!!";
-$dbname = "DB4098787";
-
 function insertNewUser($firstName, $lastName, $userName, $emailaddress)
 {
-	 global $servername, $username,$password,$dbname;
+	 require 'config.php';
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-
-    $sql = "INSERT INTO USER(FIRSTNAME, LASTNAME, DISPLAYNAME, EMAILADDRESS) 
-    VALUES ($firstName, $lastName, $userName, $emailaddress)";
-
+    // $sql = "INSERT INTO USER(FIRSTNAME, LASTNAME, DISPLAYNAME, EMAILADDRESS)
+    // VALUES ($firstName, $lastName, $userName, $emailaddress)";
+		$sql = sprintf("INSERT INTO USER(FIRSTNAME, LASTNAME, DISPLAYNAME, EMAILADDRESS)  VALUES ('%s','%s','%s','%s')",
+            mysql_real_escape_string($user),
+            mysql_real_escape_string($lastName),
+						mysql_real_escape_string($userName),
+						mysql_real_escape_string($emailadress));
     if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
+        echo "New User created successfully";
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
-
     $conn->close();
 }
 
-function insertNewCircle($circleName, $description, $frequency, $url) 
+function insertNewCircle($circleName, $description, $frequency, $url)
 {
-	 global $servername, $username,$password,$dbname;
+		require 'config.php';
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
     // Check connection
@@ -40,14 +37,16 @@ function insertNewCircle($circleName, $description, $frequency, $url)
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $sql = "INSERT INTO USER(FIRSTNAME, LASTNAME, DISPLAYNAME, EMAILADDRESS) 
-    VALUES ($circleName, $description, $frequency,$url)";
+    $sql = sprintf("INSERT INTO CIRCLE(NAME, DESCRIPTION, FREQUENCY, URL) VALUES ('%s','%s','%s','%s')",
+		mysql_real_escape_string($circleName),
+		mysql_real_escape_string($description),
+		mysql_real_escape_string($frequency),
+		mysql_real_escape_string($url));
 
     if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
+        echo "New Circle created successfully";
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
-
     $conn->close();
 }
