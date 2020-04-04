f8bfaa67dfcee660150f5d534147fdac5adbd0dc
chris Rehm <christopherrehm@web.de>
2020-04-04 16:18:59 +0200

    working test page, submits data to db

--- backendCode/testlib.php
+++ backendCode/testlib.php
@@ -1,11 +1,25 @@
 <html>
 <h1>Welcome</h1>
 <p>
-  Insert : joe schmucky joeyboy joe.schmuck@silly.net
-</p>
+  Insert a user name
+  
 <?php
-require 'functionLibrary.php';
-// function insertNewUser($firstName, $lastName, $userName, $emailaddress)
-insertNewUser('joe','schmucky','joeboy','joe.schmuck@silly.net');
+ require 'functionLibrary.php';
+ if (isset($_POST['firstName']) && isset($_POST['lastName']) && isset($_POST['userName']) && isset($_POST['emailaddress'])) {
+    //$result = addNumbers(intval($_POST['number1']), intval($_POST['number2']));
+    $result = insertNewUser($_POST['firstName'], $_POST['lastName'], $_POST['userName'], $_POST['emailaddress']);
+}
+//insertNewUser($firstName, $lastName, $userName, $emailaddress)
+//insertNewUser('joe','schmucky','joeboy','joe.schmuck@silly.net');
 ?>
+
+<form method="post">
+First Name: <input type="text" name="firstName"><br>
+Last Name: <input type="text" name="lastName"><br>
+User Name: <input type="text" name="userName"><br>
+E-mail: <input type="text" name="emailaddress"><br>
+<input type="submit">
+</form>
+</p>
+
 </html>
